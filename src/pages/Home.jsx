81d91b91905cf1a487865278c3ffad3287fe027d
Sina <sinaderik7@gmail.com>
2024-11-20 16:59:20 +0330

    button deleted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,9 @@
-
-import { Button } from "@components/ui/button";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
       <p className="text-3xl font-bold underline">Home</p>
-      <Button>test</Button>
     </div>
   );
 };
