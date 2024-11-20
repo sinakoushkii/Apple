3b9b64f81cf3edfc85d1c1d738b665637e1171bc
Sina <sinaderik7@gmail.com>
2024-11-20 16:50:24 +0330

    shadcn is configured

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+
+import { Button } from "@components/ui/button";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
       <p className="text-3xl font-bold underline">Home</p>
+      <Button>test</Button>
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // Use import instead of require
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@components': '/src/components',
-      '@pages': '/src/pages',
-      '@utils': '/src/utils',
-      '@routes': '/src/routes',
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
