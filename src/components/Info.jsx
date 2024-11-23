d8e7922ba9933e29a5c7dcf1b2b97e6747578b1e
Sina <sinaderik7@gmail.com>
2024-11-23 09:55:16 +0330

    last commit

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Info = () => {
   return (
-    <div className='w-full flex items-center justify-center bg-gray-200'>
-        <div className='container mx-auto px-7 flex items-center justify-center text-center'>
-        <p>Starting 11/29, get an Apple Gift Card up to $75 when you buy an eligible iPhone with a one‑time payment</p>
-        <a className='ml-2'>Learn More</a>
-        </div>
+    <div className="w-full flex items-center justify-center bg-gray-100 py-2">
+      <div className="container mx-auto px-7 flex items-center justify-center text-center">
+        <p className="text-sm">
+          Starting 11/29, get an Apple Gift Card up to $75 when you buy an
+          eligible iPhone with a one‑time payment{" "}
+          <a href="#" className="ml-1 text-blue-600 text-sm">
+            Learn More
+          </a>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
