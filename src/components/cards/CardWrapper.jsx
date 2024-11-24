393c0ab5bfc3b4eaceaf79057b3e4d629de65bdf
Sina <sinaderik7@gmail.com>
2024-11-24 17:14:01 +0330

    card detailes are rendered in the modal

--- src/components/cards/CardWrapper.jsx
+++ src/components/cards/CardWrapper.jsx
@@ -4,41 +4,46 @@ import { cardData } from "@/constants";
 import CardModal from "@components/CardModal";
 
 const CardWrapper = () => {
-  const body=document.getElementById("root")
-  const [showCardModal,setShowCardModal]=useState(false)
-  const [selectedCard,setSelectedCard]=useState(null)
+  const body = document.getElementById("root");
+  const [showCardModal, setShowCardModal] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(null);
 
-
-  const showCardModalHandler=(card)=>{
-    setShowCardModal(true)
-    setSelectedCard(card)
-  }
-  const closeCardModalHandler=()=>{
-    setShowCardModal(false)
-  }
+  const showCardModalHandler = (card) => {
+    setShowCardModal(true);
+    setSelectedCard(card);
+  };
+  const closeCardModalHandler = () => {
+    setShowCardModal(false);
+  };
 
   return (
-    <div className={`${showCardModal?"blur":""}mt-10 pt-6 container mx-auto px-7`}>
+    <div
+      className={`${
+        showCardModal ? "blur" : ""
+      }mt-10 pt-6 container mx-auto px-7`}
+    >
       <div className="container mx-auto px-7">
         <p className="text-3xl font-semibold md:text-5xl">Get to know iPhone</p>
       </div>
       <div className="card-wrapper mt-8 flex items-center justify-start gap-5">
         {cardData.map((item) => (
           <Card
+            key={item.id}
             title={item.title}
             subTitle={item.subTitle}
             img={item.img}
             mainImg={item.mainImg}
             description={item.description}
-            showCardModalHandler={()=>showCardModalHandler(item)}
+            showCardModalHandler={() => showCardModalHandler(item)}
           />
         ))}
-
       </div>
-      {showCardModal &&(
+      {showCardModal && (
         <CardModal card={selectedCard} onClose={closeCardModalHandler} />
       )}
-      {showCardModal ? body.classList.add("blur") :body.classList.remove("blur")}
+      {showCardModal
+        ? body.classList.add("blur")
+        : body.classList.remove("blur")}
     </div>
   );
 };
