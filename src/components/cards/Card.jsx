344feab4a499672cd2888aed5afab30cfba6c499
Sina <sinaderik7@gmail.com>
2024-11-24 14:40:52 +0330

    cards are rendered

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import plusBtn from '../../assets/icons/plus.png'
+import img1 from '../../assets/images/card1mainImage.jpg'
 
-const Card = () => {
+const Card = ({title,subTitle,img,mainImg,description}) => {
   return (
-    <div>Card</div>
+    <div>
+        <div className='card'>
+            <img className='card__main-img' src={mainImg}/>
+            <div className='card__title-wrapper'>
+                <h3 className='text-base font-semibold'>{subTitle}</h3>
+                <h2 className='text-3xl font-semibold mt-2'>{title}</h2>
+            </div>
+            <img className='card__plus-btn' width={30} height={30} src={plusBtn}/>
+        </div>
+    </div>
   )
 }
 
--- src/components/cards/CardWrapper.jsx
+++ src/components/cards/CardWrapper.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
+import { cardData } from "@/constants";
 
 const CardWrapper = () => {
   return (
-    <div>
-      CardWrapper
+    <div className="mt-10 pt-6 container mx-auto px-7">
+      <div className="container mx-auto px-7">
+        <p className="text-3xl font-semibold md:text-5xl">Get to know iPhone</p>
+      </div>
+      <div className="mt-8 flex items-center justify-start gap-5">
+        {cardData.map((item) => (
+          <Card
+            title={item.title}
+            subTitle={item.subTitle}
+            img={item.img}
+            mainImg={item.mainImg}
+            description={item.description}
+          />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardWrapper
+export default CardWrapper;
