710da6e780bfbaa3d8d7216c81b62636237e66c5
Sina <sinaderik7@gmail.com>
2024-11-24 14:52:35 +0330

    cards get scrolled horizontally

--- src/components/cards/CardWrapper.jsx
+++ src/components/cards/CardWrapper.jsx
@@ -8,7 +8,7 @@ const CardWrapper = () => {
       <div className="container mx-auto px-7">
         <p className="text-3xl font-semibold md:text-5xl">Get to know iPhone</p>
       </div>
-      <div className="mt-8 flex items-center justify-start gap-5">
+      <div className="card-wrapper mt-8 flex items-center justify-start gap-5">
         {cardData.map((item) => (
           <Card
             title={item.title}
