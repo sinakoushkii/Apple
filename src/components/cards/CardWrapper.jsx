62676f0fcee205d06c56327e5f0bafc9955aeae5
Sina <sinaderik7@gmail.com>
2024-11-25 10:17:41 +0330

    cards width is responsive now and slider is created

--- src/components/cards/CardWrapper.jsx
+++ src/components/cards/CardWrapper.jsx
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Card from "./Card";
 import { cardData } from "@/constants";
 import CardModal from "@components/CardModal";
 
 const CardWrapper = () => {
+  //states to handle the card modal
   const body = document.getElementById("root");
   const [showCardModal, setShowCardModal] = useState(false);
   const [selectedCard, setSelectedCard] = useState(null);
 
+  //states to handle the slider
+  const itemWidth = 150;
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const containerRef = useRef();
+
+  // card modal functions
   const showCardModalHandler = (card) => {
     setShowCardModal(true);
     setSelectedCard(card);
   };
   const closeCardModalHandler = () => {
     setShowCardModal(false);
+  };
+
+  const handleScroll = (scrollAmount) => {
+    const newScrollPosition = scrollPosition + scrollAmount;
+    setScrollPosition(newScrollPosition);
+    containerRef.current.scrollLeft = newScrollPosition;
   };
 
   return (
@@ -25,18 +38,27 @@ const CardWrapper = () => {
       <div className="container mx-auto px-7">
         <p className="text-3xl font-semibold md:text-5xl">Get to know iPhone</p>
       </div>
-      <div className="card-wrapper mt-8 flex items-center justify-start gap-5">
-        {cardData.map((item) => (
-          <Card
-            key={item.id}
-            title={item.title}
-            subTitle={item.subTitle}
-            img={item.img}
-            mainImg={item.mainImg}
-            description={item.description}
-            showCardModalHandler={() => showCardModalHandler(item)}
-          />
-        ))}
+      <div className="flex flex-col">
+        <div
+          ref={containerRef}
+          className="card-wrapper mt-8 flex items-center justify-start gap-5 scroll-smooth"
+        >
+          {cardData.map((item) => (
+            <Card
+              key={item.id}
+              title={item.title}
+              subTitle={item.subTitle}
+              img={item.img}
+              mainImg={item.mainImg}
+              description={item.description}
+              showCardModalHandler={() => showCardModalHandler(item)}
+            />
+          ))}
+        </div>
+        <div className="flex items-center justify-center gap-3 self-end mt-3">
+          <span onClick={()=>handleScroll(-itemWidth)}>left</span>
+          <span onClick={()=>handleScroll(itemWidth)}>right</span>
+        </div>
       </div>
       {showCardModal && (
         <CardModal card={selectedCard} onClose={closeCardModalHandler} />
